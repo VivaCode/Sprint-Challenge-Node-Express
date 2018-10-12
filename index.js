600dc6c1d35959de2722733777e69924d81399a3
Emily McClanahan <emily.f.mcclanahan@gmail.com>
2018-10-12 10:51:45 -0500

    get all endpoint for projects and actions in place and functional

--- index.js
+++ index.js
@@ -3,6 +3,46 @@ const express = require("express");
 const server = express();
 const port = 9000;
 
+server.use(express.json());
+
+const projects = require('./data/helpers/projectModel');
+const actions = require('./data/helpers/actionModel');
+
+// ** Project Routes **
+
+//get all projects
+server.get('/api/projects', (req, res) => {
+    projects
+    .get()
+    .then(projects => {
+        res.status(200).json(projects);
+    })
+    .catch(err => {
+        res.status(500).json({message: 'No Projects Found'});
+    });
+});
+
+//get projects by id
+
+
+
+// ** Action Routes **
+
+//get all actions
+server.get('/api/actions', (req, res) => {
+    actions
+    .get()
+    .then(actions => {
+        res.status(200).json(actions);
+    })
+    .catch(err => {
+        res.status(500).json({message: 'No Actions Found'});
+    });
+});
+
+
+
+
 server.listen(port, () => {
     console.log (`\n ******* Server Running on Port ${port} ********\n`)
 })

c41d7297722ee03f794d07a52dc9a5c7cf52f8e7
Emily McClanahan <emily.f.mcclanahan@gmail.com>
2018-10-12 12:06:57 -0500

    endpoint to update projects and actions functional.

--- index.js
+++ index.js
@@ -14,51 +14,68 @@ const actions = require('./data/helpers/actionModel');
 //get all projects
 server.get('/api/projects', (req, res) => {
     projects
-    .get()
-    .then(projects => {
-        res.status(200).json(projects);
-    })
-    .catch(err => {
-        res.status(500).json({message: 'No Projects Found'});
-    });
+        .get()
+        .then(projects => {
+            res.status(200).json(projects);
+        })
+        .catch(err => {
+            res.status(500).json({message: 'No Projects Found'});
+        });
 });
 
 //get projects by id
 server.get('/api/projects/:id', (req, res) => {
     const id = req.params.id;
     projects
-    .get(id)
-    .then(project => {
-        if(!project) {
-            res.status(404).json({message: 'No Project with this ID exists'})
-        } else {
-            res.status(200).send(project);
-        }
-    })
-    .catch(err => {
-        res.status(500).json({error: 'project can not be populated at this time'});
-    });
+        .get(id)
+        .then(project => {
+            if(!project) {
+                res.status(404).json({message: 'No Project with this ID exists'})
+            } else {
+                res.status(200).send(project);
+            }
+        })
+        .catch(err => {
+            res.status(500).json({error: 'project can not be populated at this time'});
+        });
 });
 
 //create new project
 server.post('/api/projects/', (req, res) => {
     const projectModel = req.body;
     projects
-    .insert(projectModel)
-    .then(response => {
-        res.status(201).json(response);
-    })
-    .catch(err => {
-        if (err.errno === 19) {
-            res.status(404).json({message: 'please provide all required fields'});
-        } else {
-            res.status(500).json({message: 'Not able to create new project at this time'});
-        }
-    });
+        .insert(projectModel)
+        .then(response => {
+            res.status(201).json(response);
+        })
+        .catch(err => {
+            if (err.errno === 19) {
+                res.status(404).json({message: 'please provide all required fields'});
+            } else {
+                res.status(500).json({message: 'Not able to create new project at this time'});
+            }
+        });
 });
 
-//update project
-
+//update project *** come back to check error handling
+server.put('/api/projects/:id', (req, res) => {
+    const id = req.params.id;
+    const update = req.body;
+    projects
+        .update(id, update)
+        .then(count => {
+            if (count > 0) {
+            projects.get(id).then(projects => {
+                res.status(200).json(projects);
+            })
+            } else {
+                res.status(400).json({message: 'Project not found: cannot update.'});   
+            }
+        })
+        .catch(err => {
+            res.status(500).json({message: 'Server cannot update project at this time.'});
+        });
+   });
 
 //delete project
 
@@ -68,30 +85,30 @@ server.post('/api/projects/', (req, res) => {
 //get all actions
 server.get('/api/actions', (req, res) => {
     actions
-    .get()
-    .then(actions => {
-        res.status(200).json(actions);
-    })
-    .catch(err => {
-        res.status(500).json({message: 'No Actions Found'});
-    });
+        .get()
+        .then(actions => {
+            res.status(200).json(actions);
+        })
+        .catch(err => {
+            res.status(500).json({message: 'No Actions Found'});
+        });
 });
 
 //get actions by id
 server.get('/api/actions/:id', (req, res) => {
     const id = req.params.id;
     actions
-    .get(id)
-    .then(action => {
-        if(!action) {
-            res.status(404).json({message: 'No Action with this ID exists'})
-        } else {
-            res.status(200).send(action);
-        }
-    })
-    .catch(err => {
-        res.status(500).json({error: 'action can not be populated at this time'});
-    });
+        .get(id)
+        .then(action => {
+            if(!action) {
+                res.status(404).json({message: 'No Action with this ID exists'})
+            } else {
+                res.status(200).send(action);
+            }
+        })
+        .catch(err => {
+            res.status(500).json({error: 'action can not be populated at this time'});
+        });
 });
 
 //create new action
@@ -106,16 +123,35 @@ server.post('/api/actions', (req, res) => {
         return res.status(400).json({msg: 'please provide all required fields'});
     }
     actions
-    .insert(newAction)
-    .then(response => {
-        res.status(201).json(response);
-    })
-    .catch(err => {
-        res.status(500).json({message: 'Not able to create new action at this time'});
-    });
+        .insert(newAction)
+        .then(response => {
+            res.status(201).json(response);
+        })
+        .catch(err => {
+            res.status(500).json({message: 'Not able to create new action at this time'});
+        });
 });
 
-//update action
+//update action by id *** come back to check error handling
+server.put('/api/actions/:id', (req, res) => {
+    const id = req.params.id;
+    const update = req.body;
+    actions
+        .update(id, update)
+        .then(count => {
+            if (count > 0) {
+                actions.get(id).then(action => {
+                    res.status(200).json(action)
+                })
+            } else {
+                res.status(400).json({message: 'Action not found: cannot update.'});   
+            }
+        })
+        .catch(err => {
+            res.status(500).json({message: 'Server cannot update project at this time.'});
+        });
+})
+
 
 //delete action
 
